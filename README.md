8fcd8803d3e6a4ab9e947fc9089e586fe7749bec
JunkMeal <65283415+JunkMeal@users.noreply.github.com>
2021-08-10 21:23:22 +0200

    Adding files

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# duolingo-plus
+># duolingo-plus
 Basically free 6 months of duolingo plus
+
+>## HOW TO USE?!?!?!?
+You need to have node.js installed with npm
+
+Then proceed to run `npm i` that installs all packages
+
+Then just start it with `node index.js`
+
+
+>## What the program doin?
+It opens headless chrome, goes to your referral link and creates x random accounts
+
+>## How to configure the config?
+```json
+{
+    "link": "https://invite.duolingo.com/codehere",
+    "count": 19,
+    "headless": true
+}
+```
+
+>### Link
+Is the referral link
+
+>### Count
+Amount of accounts that you want to create 19 recommended because that gives you 6 months of plus
+
+>### Headless
+Keep true unless you want to see chromium
---
+++ index.js
@@ -0,0 +1,103 @@
+const puppeteer = require('puppeteer');
+const config = require("./config.json");
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: config.headless,
+        args: [
+            "--disable-notifications"
+        ]
+    });
+    const page = await browser.newPage();
+
+    async function type(xpath, text) {
+        await page.waitForXPath(xpath, {
+            visible: true,
+            timeout: 0
+        });
+        const input = await page.$x(xpath)
+        if (input) await input[0].type(text);
+    }
+    async function click(xpath) {
+        await page.waitForXPath(xpath, {
+            visible: true,
+            timeout: 0
+        });
+        const button = await page.$x(xpath)
+        if (button) await button[0].click();
+
+    }
+
+    for (let i = 0; i < config.count; i++) {
+        await create();
+        console.log(`Account ${i+1} created. [${process.pid}]`)
+    }
+
+    console.log("Done！")
+    browser.close();
+
+    async function create() {
+        await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36")
+        await page.goto(config.link);
+
+        // Accept cookies
+        await click("/html/body/div[3]/div[2]/div/div[1]/div/div[2]/div/button[2]")
+
+        // Select German
+        await click("/html/body/div[1]/div/div/span/div/div/div/ul/a[4]")
+
+        // Select first in the page "Where did you hear about us" (Doesnt need to be friends)
+        await click("/html/body/div[1]/div/div/div/div[2]/div/div/div/label[1]/span");
+
+        // Continue
+        await click("/html/body/div[1]/div/div/div/div[2]/div/div/button");
+
+        // Why are you learning a language? page
+        await click("/html/body/div[1]/div/div/div/div[2]/div/div/div/ul/div[1]")
+
+        // Continue
+        await click("/html/body/div[1]/div/div/div/div[2]/div/div/button")
+
+        // Continue
+        await click("/html/body/div[1]/div/div/div/div[2]/div/div/button")
+
+        // Continue
+        await click("/html/body/div[1]/div/div/div/div[2]/div/div/button")
+
+        // Not now
+        await click("/html/body/div[1]/div/div/div/div[2]/div/div/ul/li[3]/button")
+
+        // Beginer
+        await click("/html/body/div[1]/div/div/div/div[2]/div/div/div/button[1]")
+
+        // Exit
+        await click("/html/body/div[1]/div/div/div/div/div[1]/div/div/button")
+
+        // Create profile
+        await click("/html/body/div[1]/div/div[2]/div/div/div[1]/div[2]/button[1]")
+
+        // Creating profile
+
+        let user = require("faker").helpers.createCard();
+        user.password = require("faker").internet.password();
+        user.age = "5";
+
+        // Age 5 (doesnt automaticly follow)
+        await type("/html/body/div[2]/div[5]/div/div/form/div[1]/div[1]/div[1]/label/div/input", user.age)
+
+        // Input random name
+        await type("/html/body/div[2]/div[5]/div/div/form/div[1]/div[1]/div[2]/label/div/input", user.username)
+
+        // Input random gmail
+        await type("/html/body/div[2]/div[5]/div/div/form/div[1]/div[1]/div[3]/label/div/input", user.email)
+
+        // Input random password
+        await type("/html/body/div[2]/div[5]/div/div/form/div[1]/div[1]/div[4]/label/div/input", user.password)
+
+        // Register
+        await click("/html/body/div[2]/div[5]/div/div/form/div[1]/button")
+
+        // Clear cookies
+        const client = await page.target().createCDPSession()
+        await client.send('Network.clearBrowserCookies')
+    }
+})();
