bd4f44d63cf60fffd3fe3aac1b15949145fd3994
JunkMeal <65283415+JunkMeal@users.noreply.github.com>
2023-02-25 22:47:18 +0100

    Add captcha solving

--- index.js
+++ index.js
@@ -5,12 +5,28 @@ let createAccount = async (referral) => {
     const { v4: uuidv4 } = require('uuid');
     const Chance = require("chance");
     const chance = new Chance();
-
+    const ua = `"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393"`
     const duo = axios.create({
         baseURL: "https://www.duolingo.com/2017-06-30/",
-        headers: { "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393" },
+        headers: { "User-Agent": ua },
     });
     
+    const { data } = await axios.get("https://www.recaptcha.net/recaptcha/enterprise/anchor?ar=1&k=6LcLOdsjAAAAAFfwGusLLnnn492SOGhsCh-uEAvI&co=aHR0cHM6Ly93d3cuZHVvbGluZ28uY29tOjQ0Mw..&hl=en&v=Nh10qRQB5k2ucc5SCBLAQ4nA&size=invisible&cb=ql0fzyhsvuzv", 
+    {
+        headers: {
+            "User-Agent": ua
+        }
+    })
+    const starts = `id="recaptcha-token" value="`;
+    const start = data.indexOf(starts) + starts.length;
+    const captcha_token = data.substring(start, data.indexOf(`"`, start) + 1);
+
+    const signal = {
+        siteKey: "6LcLOdsjAAAAAFfwGusLLnnn492SOGhsCh-uEAvI",
+        token: captcha_token,
+        vendor: 2
+    }
+
     let referral_code = referral.split("/");
     referral_code = referral_code[referral_code.length - 1]
     
@@ -21,7 +37,8 @@ let createAccount = async (referral) => {
             "distinctId": uuidv4(),
             "timezone": "America/Montreal",
             "fromLanguage": "en",
-            "inviteCode": referral_code
+            "inviteCode": referral_code,
+            signal
         }
     }).catch(e => {
         console.error(e)
@@ -32,11 +49,12 @@ let createAccount = async (referral) => {
 
     await duo({
         method: "PATCH",
-        url: `/users/${id}?fields=none`,
+        url: `/users/${id}?fields=email,identifier,name,username`,
         data: {
             "age": "5",
             "email": chance.email(),
-            "password": require("crypto").randomBytes(12).toString("base64")
+            "password": require("crypto").randomBytes(12).toString("base64"),
+            signal
         },
         headers: {
             Cookie: `jwt_token=${jwt.value};`
